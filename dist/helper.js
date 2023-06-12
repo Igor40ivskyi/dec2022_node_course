a9e2ea81067502bb202ce44cc6fdaa607bf8d543
Admin <sorokivskyi777@gmail.com>
2023-06-12 17:36:05 +0300

    lesson2 express

--- dist/helper.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sayHelloFunc = void 0;
-function sayHello() {
-    console.log("hello");
-}
-exports.sayHelloFunc = sayHello;
